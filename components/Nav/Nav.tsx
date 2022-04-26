785d816a58fc545b83d6089bf6ae0313fdbc182d
Bartosz Skórka <bartosz.skorka@monogo.pl>
2022-04-26 08:16:37 +0200

    active link added to navbar

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,18 +1,45 @@
 import Link from 'next/link';
 import styles from '../../styles/Nav.module.scss';
+import { useRouter } from 'next/router';
 
 export const Nav = () => {
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
       <ul>
         <li className={styles.li}>
-          <Link href='/launches'>Launches</Link>
+          <Link href='/launches'>
+            <a
+              className={
+                router.pathname === '/launches' ? `${styles.linkActive}` : ''
+              }
+            >
+              Launches
+            </a>
+          </Link>
         </li>
         <li className={styles.li}>
-          <Link href='/rockets'>Rockets</Link>
+          <Link href='/rockets'>
+            <a
+              className={
+                router.pathname === '/rockets' ? `${styles.linkActive}` : ''
+              }
+            >
+              Rockets
+            </a>
+          </Link>
         </li>
         <li className={styles.li}>
-          <Link href='/about'>About</Link>
+          <Link href='/about'>
+            <a
+              className={
+                router.pathname === '/about' ? `${styles.linkActive}` : ''
+              }
+            >
+              About
+            </a>
+          </Link>
         </li>
       </ul>
     </div>
