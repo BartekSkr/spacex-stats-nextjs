5510dad67280f66ce000e9842fc87ab3c0827ea0
Bartosz Skórka <bartosz.skorka@monogo.pl>
2022-05-27 11:39:16 +0200

    simple home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next';
 import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <h2>The app shows SpaceX launches & rockets stats</h2>
+    </div>
+  );
 };
 
 export default Home;
