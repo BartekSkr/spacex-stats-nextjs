6e41276fc672b7016520fad2087dac6fe866e783
Bartosz Skórka <bartosz.skorka@monogo.pl>
2022-09-12 12:04:14 +0200

    PWA added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/]
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     loader: 'akamai',
     path: '',
     domains: ['images2.imgbox.com', 'i.imgur.com', 'imgur.com'],
   },
-};
+});
 
 module.exports = nextConfig;
